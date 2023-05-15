bab24924c11bff790ca8a68046d53c219087e8ae
Travis Cardwell <travis.cardwell@extrema.is>
2023-05-15 16:01:06 +0900

    Use Rhyolite aa/ghc810

--- default.nix
+++ default.nix
@@ -9,9 +9,23 @@
   }
 }:
 with obelisk;
-project ./. ({ ... }: {
+project ./. ({ hackGet, pkgs, ... }: {
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
   ios.bundleName = "Obelisk Minimal Example";
+
+  overrides = pkgs.lib.composeExtensions
+    ( pkgs.callPackage (hackGet ./dep/rhyolite) {
+        inherit pkgs;
+        inherit obelisk;
+      }
+    ).haskellOverrides
+    ( self: super:
+        with pkgs.haskell.lib;
+        {
+          beam-core = dontCheck super.beam-core;
+          beam-postgres = dontCheck super.beam-postgres;
+        }
+    );
 })
